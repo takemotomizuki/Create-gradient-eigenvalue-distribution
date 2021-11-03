79b9f79d9a0f9aa29617a756f988b63188909dd2
takemotomizuki <aya.greeanapple0302@gmail.com>
2021-11-03 15:37:30 +0900

    :+1: ヒストグラム作成機能完成

--- keras_imagenet.py
+++ keras_imagenet.py
@@ -1,4 +1,3 @@
-from os import path
 import tensorflow as tf
 import numpy as np
 import ssl
@@ -34,28 +33,18 @@ def get_loss_gradient(model, _x, _y):
     
     return gradient
 
-
-def create_eige_ans(_grad_list):
-    _s_ans = None
-    for g in _grad_list:
-        U, s, V = np.linalg.svd(g.T, full_matrices=False)
-        if _s_ans is None:
-            _s_ans = s
-        else:
-            np.concatenate([_s_ans,s])
-    
-    return _s_ans
-
-
 size = 100
+eps = 0.005
+img_max = 255.0
+#ヒストグラム調整用
+adj = 500
+
 #自作したnpzファイル画像サイズは224,224で保存している
 d= np.load('../dataset/imagenet_val_float.npz')
 x = d['x'][:size]
 y = d['y'][:size]
 
-eps = 0.01
-img_max = 255.0
-
+#vgg16
 vgg16 = tf.keras.applications.vgg16.VGG16(weights='imagenet')
 x_vgg16 = copy.deepcopy(x)
 x_vgg16 = tf.keras.applications.vgg16.preprocess_input(x_vgg16)
@@ -63,39 +52,81 @@ preds_vgg16 = np.argmax(vgg16.predict(x_vgg16), axis=1)
 acc_vgg16 = np.sum(preds_vgg16 == np.argmax(y, axis=1)) / y.shape[0]
 print(acc_vgg16*100,"vgg16 clean img")
 grad_vgg16 = get_loss_gradient(vgg16, x, y)
-X_adv = copy.deepcopy(x)
-X_adv = X_adv + eps*np.sign(grad_vgg16)*img_max
-X_adv = np.clip(X_adv, 0, 255)
-X_adv = tf.keras.applications.vgg16.preprocess_input(X_adv)
-preds_vgg16_adv = np.argmax(vgg16.predict(X_adv), axis=1)
+X_adv_vgg16 = copy.deepcopy(x)
+X_adv_vgg16 = X_adv_vgg16 + eps*np.sign(grad_vgg16)*img_max
+X_adv_vgg16 = np.clip(X_adv_vgg16, 0, 255)
+X_adv_vgg16 = tf.keras.applications.vgg16.preprocess_input(X_adv_vgg16)
+preds_vgg16_adv = np.argmax(vgg16.predict(X_adv_vgg16), axis=1)
 acc_vgg16_adv = np.sum(preds_vgg16_adv == np.argmax(y, axis=1)) / y.shape[0]
 print(acc_vgg16_adv*100, "vgg16 adv img")
-"""
+U, s_vgg16, V = np.linalg.svd(tf.transpose(grad_vgg16), full_matrices=False)
+
+fig, ax = plt.subplots(1, 3,figsize=(12.0, 5.0))
+ax[0].hist(s_vgg16[0].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[0].set_title("vgg16 R")
+ax[1].hist(s_vgg16[1].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[1].set_title("vgg16 G")
+ax[2].hist(s_vgg16[2].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[2].set_title("vgg16 B")
+fig.suptitle("eps :" + str(eps) + " clean acc :" + str(acc_vgg16*100) + "%"+ " adv acc :" + str(acc_vgg16_adv*100) + "%")
+plt.subplots_adjust(left=0.05, right=0.99)
+plt.savefig("assets/vgg16_gradient" + "_size" + str(size) + ".png")
+plt.close()
+
+#vgg19
 vgg19 = tf.keras.applications.vgg19.VGG19(weights='imagenet')
 x_vgg19 = copy.deepcopy(x)
 x_vgg19 = tf.keras.applications.vgg19.preprocess_input(x_vgg19)
 preds_vgg19 = np.argmax(vgg19.predict(x_vgg19), axis=1)
 acc_vgg19 = np.sum(preds_vgg19 == np.argmax(y, axis=1)) / y.shape[0]
-print(acc_vgg19*100,"vgg19")
+print(acc_vgg19*100,"vgg19 clean img")
+grad_vgg19 = get_loss_gradient(vgg19, x, y)
+X_adv_vgg19 = copy.deepcopy(x)
+X_adv_vgg19 = X_adv_vgg19 + eps*np.sign(grad_vgg19)*img_max
+X_adv_vgg19 = np.clip(X_adv_vgg19, 0, 255)
+X_adv_vgg19 = tf.keras.applications.vgg19.preprocess_input(X_adv_vgg19)
+preds_vgg19_adv = np.argmax(vgg19.predict(X_adv_vgg19), axis=1)
+acc_vgg19_adv = np.sum(preds_vgg19_adv == np.argmax(y, axis=1)) / y.shape[0]
+print(acc_vgg19_adv*100, "vgg19 adv img")
+U, s_vgg19, V = np.linalg.svd(tf.transpose(grad_vgg19), full_matrices=False)
 
+fig, ax = plt.subplots(1, 3,figsize=(12.0, 5.0))
+ax[0].hist(s_vgg19[0].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[0].set_title("vgg19 R")
+ax[1].hist(s_vgg19[1].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[1].set_title("vgg19 G")
+ax[2].hist(s_vgg19[2].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[2].set_title("vgg19 B")
+fig.suptitle("eps :" + str(eps) + " clean acc :" + str(acc_vgg19*100) + "%"+ " adv acc :" + str(acc_vgg19_adv*100) + "%")
+plt.subplots_adjust(left=0.05, right=0.99)
+plt.savefig("assets/vgg19_gradient" + "_size" + str(size) + ".png")
+plt.close()
+
+#resnet50
 resnet50 = tf.keras.applications.resnet50.ResNet50(weights='imagenet')
 x_resnet50 = copy.deepcopy(x)
 x_resnet50 = tf.keras.applications.resnet50.preprocess_input(x_resnet50)
 preds_resnet50 = np.argmax(resnet50.predict(x_resnet50), axis=1)
 acc_resnet50 = np.sum(preds_resnet50 == np.argmax(y, axis=1)) / y.shape[0]
-print(acc_resnet50*100,"resnet50")
-"""
+print(acc_resnet50*100,"resnet50 clean img")
+grad_resnet50 = get_loss_gradient(resnet50, x, y)
+X_adv_resnet50 = copy.deepcopy(x)
+X_adv_resnet50 = X_adv_resnet50 + eps*np.sign(grad_resnet50)*img_max
+X_adv_resnet50 = np.clip(X_adv_resnet50, 0, 255)
+X_adv_resnet50 = tf.keras.applications.resnet50.preprocess_input(X_adv_resnet50)
+preds_resnet50_adv = np.argmax(resnet50.predict(X_adv_resnet50), axis=1)
+acc_resnet50_adv = np.sum(preds_resnet50_adv == np.argmax(y, axis=1)) / y.shape[0]
+print(acc_resnet50_adv*100, "resnet50 adv img")
+U, s_resnet50, V = np.linalg.svd(tf.transpose(grad_resnet50), full_matrices=False)
 
-"""
-s_ans = create_eige_ans(grad_list)
-
-plt.hist(s_ans[0],range=(-0.001,0.005),bins=100)
-plt.savefig('assets/resnet50_r.png')
+fig, ax = plt.subplots(1, 3,figsize=(12.0, 5.0))
+ax[0].hist(s_resnet50[0].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[0].set_title("resnet50 R")
+ax[1].hist(s_resnet50[1].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[1].set_title("resnet50 G")
+ax[2].hist(s_resnet50[2].reshape(-1)*adj*size,range=(-0.01,5),bins=size,density=True)
+ax[2].set_title("resnet50 B")
+fig.suptitle("eps :" + str(eps) + " clean acc :" + str(acc_resnet50*100) + "%"+ " adv acc :" + str(acc_resnet50_adv*100) + "%")
+plt.subplots_adjust(left=0.05, right=0.99)
+plt.savefig("assets/resnet50_gradient" + "_size" + str(size) + ".png")
 plt.close()
-plt.hist(s_ans[1],range=(-0.001,0.005),bins=100)
-plt.savefig('assets/resnet50_g.png')
-plt.close()
-plt.hist(s_ans[2],range=(-0.001,0.005),bins=100)
-plt.savefig('assets/resnet50_b.png')
-plt.close()
-"""
